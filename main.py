274250cd3d1b152c8fee9bc7d1c216f99579f452
brian-konr <r12922050@ntu.edu.tw>
2023-11-22 01:55:32 +0800

    feat: 🎸 complete hw but not impl UI yet

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
 import camelot
-
+import numpy as np
+from numpy.linalg import norm
+from sentence_transformers import SentenceTransformer
 
 class pdf2text:
     def __init__(self):
-        pass
-
-    def __call__(self, pdf_file):
-        tables = camelot.read_pdf(pdf_file, pages="all")
+        """
+        tables: list of parsed table string. Every element is a string for a table
+        """
+        self.tables = []
+    def __call__(self, pdf_file_path: str):
+        tables = camelot.read_pdf(pdf_file_path, pages="all")
+        print("Total tables extracted:", tables.n)
+        for table in tables:
+            self.tables.append(table.df.to_string().replace("\\n", ""))
+        return self.tables
+    def print_every_table(self, pdf_file_path: str):
+        tables = camelot.read_pdf(pdf_file_path, pages="all")
         texts = []
         for table in tables:
             texts.append(table.df.to_string())
-        text = "\n".join(texts)
+        text = "\n\n".join(texts)
         text = text.replace("\\n", "")
-        return text
+        print(text)
 
 
 class text2vector:
     def __init__(self):
-        pass
+        self.model = SentenceTransformer('distiluse-base-multilingual-cased-v2')
 
     def __call__(self, text):
-        pass
+        return self.model.encode(text)
 
 
 class cosine_sim:
@@ -28,16 +38,26 @@ class cosine_sim:
         pass
 
     def __call__(self, vector_from_table, vector_from_keyword):
-        pass
+        return np.dot(vector_from_table, vector_from_keyword) / (norm(vector_from_table)*norm(vector_from_keyword))
 
 
-def main(keyword, pdf_file):
+def main(keyword, pdf_file_path):    
+    # parse pdf tables to string
     pdf_parser = pdf2text()
-    table_text = pdf_parser(pdf_file)
-    print(table_text)
-    # return table
-
+    tables = pdf_parser(pdf_file_path)
+    # convert string to vector
+    txt2vec = text2vector()
+    vectors = []
+    for table in tables:
+        vectors.append(txt2vec(table))
+    # calculate cosine similarity
+    cos_sim = cosine_sim()
+    cosine_scores = []
+    for vector in vectors:
+        cosine_scores.append(cos_sim(vector, txt2vec(keyword)))
+    # return table with highest similarity
+    print(tables[np.argmax(cosine_scores)])
 
 if __name__ == "__main__":
-    main("keyword", "docs/1.pdf")
-    main("keyword", "docs/2.pdf")
+    # main("非監督式學習的應用", "docs/1.pdf")
+    main("多細胞生物細胞膜的流動性較大，植物細胞膜的流動性較小。", "docs/2.pdf")
